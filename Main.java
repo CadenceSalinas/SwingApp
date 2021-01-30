82852e7ad8da1329a43d3c8c829f2516681892d5
Cadence Salinas <91096084@g.mvusd.net>
2021-01-30 07:07:31 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+//three buttons, two labels, two text field, array list, image
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class Main
+{
+	public static void main (String args[])
+	{
+		JFrame frame1 = new JFrame("This is a JFrame");
+		frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame1.setSize(500,500);
+		frame1.setVisible(true);
+	}
+}//end main class
